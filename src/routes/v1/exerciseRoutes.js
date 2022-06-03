46969ccf7cbdd31386243a2d17367b69b1863c92
bovineCossack <85243268+bovineCossack@users.noreply.github.com>
2022-06-03 12:37:58 +0300

    remove redundant function in route

--- src/routes/v1/exerciseRoutes.js
+++ src/routes/v1/exerciseRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const exerciseController = require('../../controllers/exerciseController');
-const { isLoggedIn, validateToken } = require('../../tools/validationHelper');
+const { isLoggedIn } = require('../../tools/validationHelper');
 
 const exerciseRoutes = express.Router();
 
-exerciseRoutes.get('/exercises', isLoggedIn, validateToken, exerciseController.exerciseIndex);
-exerciseRoutes.post('/exercises', isLoggedIn, validateToken, exerciseController.createExercise);
-exerciseRoutes.delete('/exercises/:id', isLoggedIn, validateToken, exerciseController.deleteExercise);
+exerciseRoutes.get('/exercises', isLoggedIn, exerciseController.exerciseIndex);
+exerciseRoutes.post('/exercises', isLoggedIn, exerciseController.createExercise);
+exerciseRoutes.delete('/exercises/:id', isLoggedIn, exerciseController.deleteExercise);
 
 module.exports = {
     exerciseRoutes,
